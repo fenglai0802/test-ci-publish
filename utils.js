83def8d279c912bef4263f4aaaf0ec51183eb792
jichen <fenglai0802@163.com>
2019-03-20 01:03:55 +0800

    333

--- utils.js
+++ utils.js
@@ -2,12 +2,15 @@ const path = require('path');
 const fs = require('fs');
 
 function generateNpmrc(cwd) {
-  const npmrc = path.join(cwd, '.npmrc');
+  // const npmrc = path.join(cwd, '.npmrc');
+  const npmrc = path.join(cwd, 'npmrc');
   
   const NPM_EMAIL = process.env.NPM_EMAIL || 'fenglai0802@163.com';
   const NPM_TOKEN = process.env.NPM_TOKEN || 'fe514779-0279-427a-8778-391245b39aae';
   const NPM_REGISTRY = process.env.NPM_REGISTRY || 'registry.npmjs.com';
   
+  console.log(NPM_EMAIL)
+  console.log(NPM_TOKEN)
   const registry = `https://${NPM_REGISTRY}`;
   
   const npmrcContext = `email=${NPM_EMAIL}
